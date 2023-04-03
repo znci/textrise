ae70505f9446839b7952746467d1360d127c8239
zNotChill <znotchill@znci.dev>
2023-04-03 15:35:30 +0100

    push code

---
+++ src/js/index.js
@@ -0,0 +1,64 @@
+
+const style = document.createElement("style");
+style.innerHTML = `
+	.rising-text {
+		overflow: hidden;
+		white-space: nowrap;
+		text-align: center;
+		height: fit-content;
+		font-size: 2rem;
+		font-family: "Roboto", sans-serif;
+	}
+	@keyframes rising-text {
+		from {
+			transform: translate3d(0, 25px, 0);
+			opacity: 0;
+		}
+		to {
+			transform: none;
+			opacity: 1
+		}
+	}
+`
+document.head.appendChild(style);
+
+document.querySelectorAll(".rising-text").forEach((v) => {
+	let att = v.attributes;
+	let type = att.getNamedItem("data-type").value;
+	let activate = att.getNamedItem("data-activate").value;
+
+	const types = ["word", "char"];
+	if(!types.includes(type)) throw new Error("Invalid type");
+
+	const activateTypes = ["hover", "click", "onload"];
+	if(!activateTypes.includes(activate)) throw new Error("Invalid activate type");
+
+	let duration = 0.2;
+	function animate() {
+		if(type === "word") {
+			const split = v.innerText.split(" ");
+
+			split.forEach((e, i) => {
+				style.innerHTML += `
+					.rising-text-el:nth-child(${i + 1}) {
+						animation-duration: ${(duration * i) + duration}s;
+						animation-fill-name: both;
+						animation-name: rising-text;
+						display: inline-block;
+					}
+				`
+				split[i] = `<span class="rising-text-el">${e}</span>`
+			})
+
+			v.innerHTML = split.join(" ");
+		}
+	}
+
+	if(activate == "onload") {
+		window.onload = animate();
+	} else if(activate === "hover") {
+		v.addEventListener("mouseenter", animate);
+	} else if(activate === "click") {
+		v.addEventListener("click", animate);
+	}
+})
